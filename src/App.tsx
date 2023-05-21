b985df44a5e45eb814c00cf8b0ec4bde146b059f
HenriqueSenaDev <acopsenadeveloper@gmail.com>
2023-05-21 08:34:38 -0300

    QuestionsPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
+import Questions from './pages/questions';
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/questions/infinity' element={<Questions />}/>
       </Routes>
     </Router>
   );
---
+++ src/pages/questions/index.tsx
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import Button from '../../components/button';
+import Timer from './components/timer';
+import TimeTable from './components/timetable';
+import './styles.css';
+
+function Questions() {
+  const [isRunning, setIsRunning] = useState<boolean>(false);
+
+  return (
+    <div className='questions-wrapper'>
+      <div className='questions-container'>
+        <div className='questions-header'>
+          <h1>Questões Infinitas</h1>
+
+          <p>Enem 1º dia (com 1h30 de Redação) - 2m30s</p>
+        </div>
+
+        <div className='timer-area'>
+          <Timer />
+
+          <TimeTable />
+
+          <div className='timer-buttons'>
+            {isRunning ? (
+              <div className='timer-options-area'>
+                <div className='options-row'>
+                  <Button
+                    text='Resetar questão'
+                    shortcut='R'
+                    onClick={() => {}}
+                  />
+
+                  <Button
+                    text='Próxima questão'
+                    shortcut='Space Bar'
+                    onClick={() => {}}
+                  />
+                </div>
+
+                <div className='options-row'>
+                  <Button
+                    text='Esconder tabela'
+                    shortcut='T'
+                    onClick={() => {}}
+                  />
+
+                  <Button
+                    text='Finalizar'
+                    primary
+                    shortcut='Enter'
+                    onClick={() => {}}
+                  />
+                </div>
+              </div>
+            ) : (
+              <Button
+                text='Iniciar'
+                primary
+                shortcut='Enter'
+                onClick={() => {}}
+              />
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Questions;
