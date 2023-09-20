967a5434b19aca5b19ff0b033de973f6252cd500
HenriqueSenaDev <acopsenadeveloper@gmail.com>
2023-09-20 11:13:41 -0300

    TimerContext created

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { TimerContextProvider } from './contexts/timer';
 import Home from './pages/home';
 import Questions from './pages/questions';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/questions/infinity' element={<Questions />}/>
-      </Routes>
-    </Router>
+    <TimerContextProvider>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/questions/infinity' element={<Questions />}/>
+        </Routes>
+      </Router>
+    </TimerContextProvider>
   );
 }
 
