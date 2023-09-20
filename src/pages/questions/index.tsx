7b67e79195f84a4b13a6323ee6f3ad571731ea91
HenriqueSenaDev <acopsenadeveloper@gmail.com>
2023-09-20 11:18:31 -0300

    Questions page refactor

--- src/pages/questions/index.tsx
+++ src/pages/questions/index.tsx
@@ -1,43 +1,26 @@
-import { useEffect, useState } from 'react';
+import { useContext, useState } from 'react';
+import { TimerContext } from '../../contexts/timer';
 import QuestionTimeModal from './components/question-time-modal';
 import TimerArea from './components/timer-area';
 import './styles.css';
 
 function Questions() {
   const [isTimeModalOpen, setIsTimeModalOpen] = useState<boolean>(true);
-  const [questionSpecification, setQuestionSpecification] = useState<string | null>(null);
-  const [milissecondsPerQuestion, setMilissecondsPerQuestion] = useState<number | null>(null);
 
-  function getMilissecondsPerQuestion() {
-    const timeString = questionSpecification!.split('-')[2];
-    const timeWithoutExtraNote = timeString.split('(')[0];
-    const [minutes, seconds] = timeWithoutExtraNote.split('m');
-
-    setMilissecondsPerQuestion((Number(minutes) * 60 + Number(seconds)) * 1000);
-  }
-
-  useEffect(() => {
-    if (questionSpecification) getMilissecondsPerQuestion();
-  }), [questionSpecification];
+  const { timeOptionInfo } = useContext(TimerContext);
 
   return (
     <div className='questions-wrapper'>
-      {isTimeModalOpen && (
-        <QuestionTimeModal
-          setIsTimeModalOpen={setIsTimeModalOpen}
-          questionSpecification={questionSpecification}
-          setQuestionSpecification={setQuestionSpecification}
-        />
-      )}
+      {isTimeModalOpen && <QuestionTimeModal setIsTimeModalOpen={setIsTimeModalOpen}/>}
 
       <div className='questions-container'>
         <div className='questions-header'>
           <h1>Questões Infinitas</h1>
 
-          <p>{questionSpecification || 'Indefinido'}</p>
+          <p>{timeOptionInfo?.timeSpecification || 'Indefinido'}</p>
         </div>
 
-        <TimerArea milissecondsPerQuestion={milissecondsPerQuestion} />
+        <TimerArea />
       </div>
     </div>
   );
