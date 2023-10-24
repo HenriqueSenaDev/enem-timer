fec7237fed78b7409b485649048a59e871fc0066
HenriqueSenaDev <acopsenadeveloper@gmail.com>
2023-10-23 21:04:05 -0300

    feature/timer: TimeTable component moved

---
+++ src/pages/questions/components/timer-area/components/timetable/index.tsx
@@ -0,0 +1,44 @@
+import { ReactComponent as HideTableVector } from '../../../../../../assets/hide-table.svg';
+import './styles.css';
+
+interface IProps {
+  questionsTime: {
+    overall: string;
+    current: string;
+  }[];
+  isTimeHidden: boolean;
+}
+
+function TimeTable({ questionsTime, isTimeHidden }: IProps) {
+  function getTimeColor(timeString: string) {
+    return timeString.at(0) === '+' ? 'green' : 'red';
+  }
+
+  return (
+    <div className='time-table-card'>
+          <div className='table-header'>
+            <span>Questão</span>
+
+            <div className='vertical-line' />
+
+            <span>Geral</span>
+          </div>
+
+          {isTimeHidden
+            ? <HideTableVector className='hide-table' />
+            : questionsTime.map(({ current, overall }, index) => (
+              <div
+                key={index}
+                className='question-time'
+              >
+                <span className={getTimeColor(current)}>{current}</span>
+
+                <span className={getTimeColor(overall)}>{overall}</span>
+              </div>
+            ))
+          }
+    </div>
+  );
+}
+
+export default TimeTable;
