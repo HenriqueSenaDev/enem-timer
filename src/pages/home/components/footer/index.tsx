5ba3269a643f74ed37e5a4c259215bdd2651c159
HenriqueSenaDev <acopsenadeveloper@gmail.com>
2024-02-02 13:14:35 -0300

    hotfix/ui: footer utility classes

--- src/pages/home/components/footer/index.tsx
+++ src/pages/home/components/footer/index.tsx
@@ -1,11 +1,9 @@
-import './styles.css';
-
 function Footer() {
   return (
-    <footer>
-      <div className='footer-gradient' />
+    <footer className='flex items-center justify-center w-full relative mt-[30px] pt-[14px] px-9 pb-[18px] bg-footer-gradient md:px-[18px] py-[38px]'>
+      <div className='h-[60px] w-full absolute top-[-60px] bg-footer-blend' />
 
-      <p>
+      <p className="text-[10px] font-light font-['Open_Sans'] md:text-[13px]">
         © {new Date().getFullYear()} HenriqueSenaDev. Todos os Direitos
         Reservados.
       </p>
