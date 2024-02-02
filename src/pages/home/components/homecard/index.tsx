e903759b9239cfb58faa1f07b9724f2db3617765
HenriqueSenaDev <acopsenadeveloper@gmail.com>
2024-02-02 13:14:48 -0300

    hotfix/ui: HomeCard utility classes

--- src/pages/home/components/homecard/index.tsx
+++ src/pages/home/components/homecard/index.tsx
@@ -1,6 +1,5 @@
 import { CSSProperties } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './styles.css';
 
 interface IHomeCardProps {
   header: string;
@@ -15,21 +14,25 @@ function HomeCard({ header, imgSource, items, linkTo, style }: IHomeCardProps) {
 
   return (
     <div
-      className='home-card'
+      className='rounded-2xl py-9 px-8 w-[270px] h-[340px] cursor-pointer bg-card hover:bg-card-hover md:w-[300px] md:h-[380px] lg:h-[355px] lg:[&:nth-of-type(2)]:w-[290px] lg:[&:nth-of-type(2)]:-translate-y-[30px]'
       style={style || {}}
       onClick={() => navigate(linkTo)}
     >
-      <h1>{header}</h1>
+      <h1 className='text-xl font-medium md:text-[22px]'>
+        {header}
+      </h1>
 
-      <div className='home-card-img-area'>
-        <div className='home-card-blur' />
+      <div className='mt-[18px] mb-[26px] relative'>
+        <div className='bg-[rgba(40,205,241,0.25)] absolute left-[34%] rounded-[50%] w-[67px] aspect-square blur-[6px]' />
 
-        <img src={imgSource} alt={header} />
+        <img className='w-16 aspect-square m-auto' src={imgSource} alt={header} />
       </div>
 
-      <ul className='home-card-items'>
+      <ul className='flex items-center justify-center flex-col gap-5'>
         {items.map(item => (
-          <li key={item}>• {item}</li>
+          <li key={item} className='text-[15px] md:text-lg'>
+            • {item}
+          </li>
         ))}
       </ul>
     </div>
