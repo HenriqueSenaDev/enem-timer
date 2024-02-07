9939982d0065e72262d1512c0e529f85d307e45e
HenriqueSenaDev <acopsenadeveloper@gmail.com>
2024-02-07 14:03:42 -0300

    hotfix/ui: Button tailwindcss and style

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,19 +1,23 @@
 import { ComponentProps } from 'react';
-import './styles.css';
 
 interface IProps extends ComponentProps<'button'> {
   text: string;
   shortcut?: string;
 }
 
-function Button({ text, shortcut, ...rest }: IProps) {
+function Button({ text, shortcut, className, ...rest }: IProps) {
   return (
-    <div className='button-wrapper'>
-      <button {...rest}>
+    <div className='flex flex-col items-center justify-center w-[50%] lg:gap-[6px]'>
+      <button
+        className={`${className} text-[11px] bg-[#13174B] py-[7px] px-[10px] rounded-lg max-w-[115px] text-white border-0 cursor-pointer w-full font-['Poppins'] lg:text-[12.5px] lg:py-[9px] lg:max-w-[140px]`}
+        {...rest}
+      >
         {text}
       </button>
 
-      <span>{shortcut}</span>
+      <span className="hidden lg:font-['Poppins'] lg:block lg:text-sm lg:text-white lg:opacity-50 tracking-[0.5px]">
+        {shortcut}
+      </span>
     </div>
   );
 }
